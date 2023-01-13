45e60996bde4de24032a32b0a819e99b55241ece
Rodrigo Oliveira <rodrigocerqueira@outlook.com>
2023-01-12 21:40:51 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,8 +12,12 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
+		
 		Seller seller = new Seller(21, "João", "joao@gmail.com", new Date(), 3200.0, obj);
+		
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 	}
 
